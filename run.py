efa878149e6d9e667e83f841eb845b87b00c6f5d
sandibasagic <sandi.basagic@gmail.com>
2022-04-24 08:10:21 +0000

    Add main function to run the game with welcome message

--- run.py
+++ run.py
@@ -4,3 +4,13 @@ from words import word_list
 def get_word():
     word = random.choice(word_list)
     return word.upper()
+
+def play(word):
+    word_completion = "_" * len(word)
+    guessed = False
+    guessed_letters = []
+    guessed_words = []
+    tries = 6
+    print("Welcome to guess game of musical instruments!")
+    print(word_completion)
+    print("\n")
